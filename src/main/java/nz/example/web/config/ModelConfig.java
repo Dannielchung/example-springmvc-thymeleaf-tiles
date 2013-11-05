10d88c27a8e0491c31ed009732b77ab4c6153a00
nikola.zivkov <nikola.zivkov@gmail.com>
2013-11-05 08:29:23 +0100

    Initial implementation.

---
+++ src/main/java/nz/example/web/config/ModelConfig.java
@@ -0,0 +1,10 @@
+package nz.example.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("nz.example.web.model")
+public class ModelConfig {
+
+}

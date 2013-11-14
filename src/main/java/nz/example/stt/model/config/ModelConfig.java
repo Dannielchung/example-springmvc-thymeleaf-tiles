2f5d0392c84fc127b29cce8f2f239c64f7d30c30
nikola.zivkov <nikola.zivkov@gmail.com>
2013-11-14 22:49:30 +0100

    Refactored project structure.

---
+++ src/main/java/nz/example/stt/model/config/ModelConfig.java
@@ -0,0 +1,9 @@
+package nz.example.stt.model.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("nz.example.stt.model")
+public class ModelConfig {
+}
--- src/main/java/nz/example/web/config/ModelConfig.java
+++
@@ -1,9 +0,0 @@
-package nz.example.web.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("nz.example.web.model")
-public class ModelConfig {
-}

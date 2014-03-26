def5fc8c51691f6060d48cd9e7b541bbe1c7006c
nikola.zivkov <nikola.zivkov@gmail.com>
2014-03-26 08:49:32 +0100

    Added @EnableWebMvc to the webapp configuration.

--- src/main/java/nz/example/stt/webapp/config/WebAppConfig.java
+++ src/main/java/nz/example/stt/webapp/config/WebAppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.extras.tiles2.dialect.TilesDialect;
 import org.thymeleaf.extras.tiles2.spring.web.configurer.ThymeleafTilesConfigurer;
 import org.thymeleaf.extras.tiles2.spring.web.view.ThymeleafTilesView;
@@ -13,6 +14,7 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("nz.example.stt.webapp")
 public class WebAppConfig {
 

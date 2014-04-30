387f40da94d591ba8072d1c1176874d3ec7dc797
nikola.zivkov <nikola.zivkov@gmail.com>
2014-04-30 10:41:51 +0200

    Removed unnecessary empty model.

--- src/main/java/nz/example/stt/model/config/ModelConfig.java
+++
@@ -1,9 +0,0 @@
-package nz.example.stt.model.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("nz.example.stt.model")
-public class ModelConfig {
-}
